28abff22a3f87cf01b7ac1ff465a6f64d1646c47
connor-r78 <155401101+connor-r78@users.noreply.github.com>
2025-05-14 20:59:45 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   
     try {
       // Send data to a mock API (you can replace the URL with your own endpoint)
-      const response = await fetch('https://68.228.133.220:5163/api/posts', {
+      const response = await fetch('http://127.0.0.1:5163/api/posts', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(data),
